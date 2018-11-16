f003f8f865c198bba43278323005aa45007294f5
mahmoodahmad100 <mahmood073@gmail.com>
2018-11-16 23:31:38 +0200

    update

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :description, presence: true
+  validates :user, presence: true	
 end
--- test/factories/recipes.rb
+++ test/factories/recipes.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :recipe do
-    
+    name "test meal"
+    description "best egyptian meal 😂😂😂"
+    association(:user)    
   end
 end
